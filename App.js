13a017fc61234eca071154be51a1c300364f5a49
Alessandro Ryo <alessandroryo@gmail.com>
2023-01-07 00:17:20 +0100

    Workflow v0.4.0

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World! in Development</Text>
+      <Text>Hello World! in Development 2</Text>
       <StatusBar style="auto" />
     </View>
   );
